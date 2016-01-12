acc2b1019903dd1dac0588df49258078e7aecf40
Evegenn <demerbrise@gmail.com>
2016-01-12 17:21:15 +0300

    event with button

--- HTML5AppForCoursera/public_html/js/myscript.js
+++ HTML5AppForCoursera/public_html/js/myscript.js
@@ -5,10 +5,16 @@
  */
 
 function setDate(){
-                document.getElementById('date').innerHTML=Date();
-            }
+    document.getElementById('date').innerHTML=Date();
+    }
             
 function message(msg){
-  		document.getElementById('output').innerHTML = msg + " event";
-	    }
+    document.getElementById('output').innerHTML = msg + " event";
+    }
+            
+function FunctionFromw3schools() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px"; 
+    x.style.color = "red";
+    }
 
