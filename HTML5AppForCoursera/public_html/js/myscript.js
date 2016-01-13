8be3a8d02091afe849302b18a51808b2dbf073bc
Evegenn <demerbrise@gmail.com>
2016-01-13 17:38:07 +0300

    just working by Coursera with Colleen van Lent Ph.D.)

--- HTML5AppForCoursera/public_html/js/myscript.js
+++ HTML5AppForCoursera/public_html/js/myscript.js
@@ -18,4 +18,22 @@ function FunctionFromw3schools() {
     x.style.fontSize = "25px"; 
     x.style.color = "red";
     }
+function closeMe(){
+      // Find the element
+   		x=document.getElementById("demo");
+       //Option 1: Change the style attribute directly
+   		//x.style.display="none";
+  
+      //Option 2: Change the class
+   		 x.className="closed";
+	}
 
+function openMe(){
+      // Find the element
+   		x=document.getElementById("demo");
+      //Option 1: Change the style attribute directly
+   		 //x.style.display="block";
+      
+      //Option 2: Change the class
+   		 x.className="open";
+	}
