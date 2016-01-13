56c54b40e871fffeb29872046015ffe28ef67bc3
Evegenn <demerbrise@gmail.com>
2016-01-13 18:02:03 +0300

    create a template for a Assignment: JavaScript Interactive Photo Gallery by Coursera with Colleen van Lent Ph.D.

---
+++ PhotoGalleryCoursera/public_html/js/gallery.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
+    
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+  
+	}
+
+	function unDo(){
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+		
+	}
+
