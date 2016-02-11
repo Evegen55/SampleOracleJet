d22930b2f16e4b13bd8e4ae572e70f8b07995b50
Evegenn <demerbrise@gmail.com>
2016-02-11 14:19:16 +0300

    start working on a homework

---
+++ PhotoGalleryCoursera/public_html/js/myFormHomework.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*Add the JavaScript here for the function billingFunction().  
+ * It is responsible for setting and clearing the fields in Billing Information */
+
