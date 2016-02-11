042f2874a58e5aacc7bad86d85e8d116416e5eea
Evegenn <demerbrise@gmail.com>
2016-02-11 13:50:10 +0300

    add a simple form for verification data from input field

--- PhotoGalleryCoursera/public_html/js/myForm.js
+++ PhotoGalleryCoursera/public_html/js/myForm.js
@@ -5,3 +5,12 @@
  */
 
 
+function nicknameFunction(){
+    if (yesNick.checked){
+        nick.style.display="inline";
+        nickname.setAttribute('required',true);
+    }else{ 
+        nickname.removeAttribute('required');
+        nick.style.display="none";
+        }
+    }
