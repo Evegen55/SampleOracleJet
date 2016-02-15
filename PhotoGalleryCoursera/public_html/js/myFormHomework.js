634ae192d891877de719bcc8ac670d2bea6cb070
Evegenn <demerbrise@gmail.com>
2016-02-15 12:36:55 +0300

    The last homework has been finished

--- PhotoGalleryCoursera/public_html/js/myFormHomework.js
+++ PhotoGalleryCoursera/public_html/js/myFormHomework.js
@@ -7,3 +7,24 @@
 /*Add the JavaScript here for the function billingFunction().  
  * It is responsible for setting and clearing the fields in Billing Information */
 
+function billingFunction(){
+    if (same.checked){
+        var nameValue = document.getElementById("shippingName").value;
+        var zipValue = document.getElementById("shippingZip").value;
+        document.getElementById("billingName").value = nameValue;
+        document.getElementById("billingZip").value = zipValue;
+    }else{
+        document.getElementById("billingName").value = "";
+        document.getElementById("billingZip").value = "";
+       }
+    }
+    
+function billingFunctionAgain(){
+    if (same.checked){
+        billingName.value = shippingName.value;
+        billingZip.value = shippingZip.value;
+    }else{
+       billingName.value = "";
+       billingZip.value = "";
+       }
+    }
